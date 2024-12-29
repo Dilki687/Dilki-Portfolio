5b773d571fd0d2ae23483c032a239cb916a7d8c6
Dilki687 <chamodidilki44@gmail.com>
2024-12-30 00:19:27 +0530

    Hero section

--- starter-template/src/sections/Header.tsx
+++ starter-template/src/sections/Header.tsx
@@ -1,6 +1,7 @@
 export const Header = () => {
   return (
     <div className="flex justify-center items-center relative top-3">
+      {/* above relative would be relative or fixed */}
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">
           Home
